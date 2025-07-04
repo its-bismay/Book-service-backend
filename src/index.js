6792d2d6cc084737886a428ce378a809f6793263
its-bismay <2023ugee070@nitjsr.ac.in>
2025-07-04 23:40:30 +0530

    cors code updated

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
 
 app.get("/", (req,res) => {
     res.send("server is running")
