75330572321e4ecf1d49bbdd880daa64d2509986
its-bismay <2023ugee070@nitjsr.ac.in>
2025-07-05 22:25:18 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-import express from "express"
-import  "dotenv/config.js";
-import cors from "cors"
-import authRoute from './Routes/authRoute.js'
-import bookRoute from './Routes/bookRoute.js'
+import express from "express";
+import "dotenv/config.js";
+import cors from "cors";
+import authRoute from "./Routes/authRoute.js";
+import bookRoute from "./Routes/bookRoute.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 const app = express();
 
 const port = process.env.PORT || 8080;
+job.start();
 app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  }));
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
-app.get("/", (req,res) => {
-    res.send("server is running")
-})
-app.use('/api/auth', authRoute);
-app.use('/api/books', bookRoute);
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+app.use("/api/auth", authRoute);
+app.use("/api/books", bookRoute);
 
 app.listen(3000, () => {
-    console.log(`server is running on port ${port}`);
-    connectDB()
-
-})
+  console.log(`server is running on port ${port}`);
+  connectDB();
+});
