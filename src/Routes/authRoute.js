6bd5f31e44ef6710f5d116df6f0de62822e3e608
its-bismay <2023ugee070@nitjsr.ac.in>
2025-07-16 22:25:48 +0530

    createdAt field added in respones of auth routes for frontend styling

--- src/Routes/authRoute.js
+++ src/Routes/authRoute.js
@@ -63,6 +63,7 @@ router.post("/register", async (req, res) => {
         username: user.username,
         email: user.email,
         profileImage: user.profileImage,
+        createdAt: user.createdAt
       },
     });
   } catch (error) {
@@ -110,6 +111,7 @@ router.post("/login", async (req, res) => {
           username: user.username,
           email: user.email,
           profileImage: user.profileImage,
+          createdAt: user.createdAt
         },
       });
   } catch (error) {
